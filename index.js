66345c59fad8ee975f0a8f42933a70d76d73e98e
acltearr <burroughskaelen@gmail.com>
2021-04-23 11:20:47 -0400

    Added comments, modified elevator status PUT to use API returned string instead of hardcoded string

--- index.js
+++ index.js
@@ -20,76 +20,98 @@ function fallback(agent) {
 
 // Function to call the full greeting API
 function greeting(agent) {
+    // Calling the greeting API controller that gets all the info and formats it into a string that gets returned
     return axios.get(`https://rest-api-burroughs.herokuapp.com/api/greetings`)
     .then((result) => {
+        // Setting the returned string to the agent to be shown to the user
         agent.add(result.data);
     });
 }
 
 // Broken down calls of what is returned in the greeting
 function elevatorCount(agent) {
+    // Calling the API to get the amount of elevators in the table
     return axios.get(`https://rest-api-burroughs.herokuapp.com/api/elevators/count`)
     .then((result) => {
+        // Adding the returned number to the string to show the user
         agent.add('There are ' + result.data + ' elevators currently deployed.');
     });
 }
 
 function buildingCount(agent) {
+    // Calling the API to get the amount of buildings in the table
     return axios.get(`https://rest-api-burroughs.herokuapp.com/api/buildings/count`)
     .then((result) => {
+        // Adding the returned number to the string to show the user
         agent.add('There are currently ' + result.data + ' buildings served by Rocket Elevators.');
     });
 }
 
 function customerCount(agent) {
+    // Calling the API to get the amount of customers in the table
     return axios.get(`https://rest-api-burroughs.herokuapp.com/api/customers/count`)
     .then((result) => {
+        // Adding the returned number to the string to show the user
         agent.add('Rocket Elevators currently serves ' + result.data + ' customers.');
     });
 }
 
 function batteryCount(agent) {
+    // Calling the API to get the amount of batteries in the table
     return axios.get(`https://rest-api-burroughs.herokuapp.com/api/batteries/count`)
     .then((result) => {
+        // Adding the returned number to the string to show the user
         agent.add('There are currently ' + result.data + ' Rocket Elevators batteries in service.');
     });
 }
 
 function elevatorsNotRunning(agent) {
+    // Calling the API to get the amount of elevators that are not "Active" in the table
     return axios.get(`https://rest-api-burroughs.herokuapp.com/api/elevators/inactive-count`)
     .then((result) => {
+        // Adding the returned number to the string to show the user
         agent.add('There are currently ' + result.data + ' elevators not active.');
     });
 }
 
 function quoteCount(agent) {
+    // Calling the API to get the amount of quotes in the table
     return axios.get(`https://rest-api-burroughs.herokuapp.com/api/quotes/count`)
     .then((result) => {
+        // Adding the returned number to the string to show the user
         agent.add('There are currently ' + result.data + ' quotes awaiting processing.');
     });
 }
 
 function leadCount(agent) {
+    // Calling the API to get the amount of leads in the table
     return axios.get(`https://rest-api-burroughs.herokuapp.com/api/leads/count`)
     .then((result) => {
+        // Adding the returned number to the string to show the user
         agent.add('There are currently ' + result.data + ' leads in the contact requests.');
     });
 }
 
 // Function to get the status of a specified elevator
 function getElevatorStatus(agent) {
-    const id = agent.parameters.id;
+    // Setting the id taken from the user input to a variable
+    var id = agent.parameters.id;
+    // Calling the API to return the status of the given elevator
     return axios.get(`https://rest-api-burroughs.herokuapp.com/api/elevators/${id}/status`)
     .then((result) => {
+        // Setting a string to notify the user that the status was chagned
         agent.add(`The status of elevator ${id} is: ` + result.data);
     });
 }
 
 // Function to get the info of a specified elevator
 function getElevatorInfo(agent) {
-    const id = agent.parameters.id;
+    // Setting the id taken from the user input to a variable
+    var id = agent.parameters.id;
+    // Calling the API to get a specific elevator object
     return axios.get(`https://rest-api-burroughs.herokuapp.com/api/elevators/${id}`)
     .then((result) => {
+        // Setting all of results from the API to variables to be passed to the string to return to the user
         var serial_number = result.data.serial_number;
         var model = result.data.model;
         var building_type = result.data.building_type;
@@ -102,6 +124,7 @@ function getElevatorInfo(agent) {
         var created_at = result.data.updated_at;
         var updated_at = result.data.updated_at;
         var column_id = result.data.column_id;
+        // Creating the string of elevator info to return to the user
         agent.add(`Elevator ${id}'s info:\n\
             Serial Number: ${serial_number}\n\
             Model: ${model}\n\
@@ -120,26 +143,30 @@ function getElevatorInfo(agent) {
 
 // Function to change the status of a specified elevator
 function changeElevatorStatus(agent) {
-    const id = agent.parameters.id;
-    const newStatus = agent.parameters.newStatus;
-
+    // Taking the id and requested status of the elevator from the user input
+    var id = agent.parameters.id;
+    var newStatus = agent.parameters.newStatus;
+    // Calling the API with a PUT request to modify the status of the elevevator, and setting the JSON for the request
     return axios.put(`https://rest-api-burroughs.herokuapp.com/api/elevators/${id}/status`, {
         "id": `${id}`, 
         "status": `${newStatus}`
         })
-            .then(agent.add(`Elevator ${id}'s status has been changed to ${newStatus}.`)
-    );
+        .then((result) => {
+            // Setting the agent to the string returned from the API to notify the user of the cahnge
+            agent.add(result.data);
+        });
 }
 
 // Function to submit an intervention
 function submitIntervention(agent) {
-    const customer_id = agent.parameters.customer_id;
-    const building_id = agent.parameters.building_id;
-    const battery_id = agent.parameters.battery_id;
-    const column_id = agent.parameters.column_id;
-    const elevator_id = agent.parameters.elevator_id;
-    const report = agent.parameters.report;
-
+    // Setting the agent parameters given by the user to variables to be given ot the JSON
+    var customer_id = agent.parameters.customer_id;
+    var building_id = agent.parameters.building_id;
+    var battery_id = agent.parameters.battery_id;
+    var column_id = agent.parameters.column_id;
+    var elevator_id = agent.parameters.elevator_id;
+    var report = agent.parameters.report;
+    // Calling the API to POST an intervention with the given parameters
     return axios.post(`https://rest-api-burroughs.herokuapp.com/api/interventions`, {
         "customer_id": `${customer_id}`,
         "building_id": `${building_id}`,
@@ -149,6 +176,7 @@ function submitIntervention(agent) {
         "report": `${report}`,
         "status": "Pending"
         })
+        // Setting a string to return to the user confirming their submission 
         .then(agent.add(`Your intervention request has been submitted. An agent of Rocket Elevators will contact you soon.`)
     );
 }
